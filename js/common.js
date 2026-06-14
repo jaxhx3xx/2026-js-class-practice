541cba61a5a4236996c9f7f7ee58699c1942cbba
jaxhx3xx <jaehuij660@gmail.com>
2026-06-15 00:07:00 +0900

    실습 코드 업로드

---
+++ js/common.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * @param {*} productPrice
+ * @returns
+ */
+function taxAmount(productPrice) {
+  const tax = 0.1;
+  return productPrice * tax;
+}
+/**
+ *
+ * @returns
+ */
+function getTop5() {
+  return ["정재희", 2, 3, 4, 5];
+}
